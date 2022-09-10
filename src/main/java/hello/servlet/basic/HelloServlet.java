a3fefa150e0b3926a32fdcbce8b75197347c4c73
yongckim <zxcv9203@naver.com>
2022-09-11 01:20:41 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,9 @@
+package hello.servlet.basic;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class HelloServlet extends HttpServlet {
+
+}
